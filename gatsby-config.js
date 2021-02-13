f0e962962395048892c21d9523a860030a427abf
Kumar Gaurav <kmrgrv119@gmail.com>
2021-02-13 13:18:43 +0530

    Integrating Contentful and setting up environment variables using dotenv

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,14 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
+require('dotenv/config')
 
 module.exports = {
-  /* Your site config here */
-  plugins: [
-    'gatsby-plugin-sass'
-  ],
+    plugins: [
+        "gatsby-plugin-sass",
+        {
+            resolve: "gatsby-source-contentful",
+            options: {
+                spaceId:     process.env.CONTENTFUL_SPACE_ID,
+                accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+            }
+        }
+    ]
 }
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,13 +1,27 @@
 import React from 'react'
+import { graphql, useStaticQuery } from 'gatsby'
 
 import '../sass/main.scss'
 import Layout from '../components/layout'
 
 const BlogPage = () => {
+    const data = useStaticQuery(graphql`
+        query {
+            allContentfulBlogPost {
+                edges {
+                    node {
+                        title
+                    }
+                }
+            }
+        }
+    `)
+
     return (
         <div>
             <Layout>
-                Blog
+                <h1>Blog</h1>
+                {data.allContentfulBlogPost.edges.map((edge) => <p>{edge.node.title}</p>)}
             </Layout>
         </div>
     )
